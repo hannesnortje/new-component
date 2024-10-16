bd87de5161d93ed4d7f7677c7b5d3323dd901d70
Hannes Nortje <nortje.johannes@gmail.com>
2024-10-16 21:43:52 +0200

    Directory selection

--- src/extension.ts
+++ src/extension.ts
@@ -56,121 +56,132 @@ function loadTemplate(version: string, templateFileName: string): string {
     }
 }
 
-// Function to create component files and folder structure in the workspace
-async function createComponentFiles(name: string, version: string, description: string) {
-    const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (workspaceFolders) {
-        const rootPath = workspaceFolders[0].uri.fsPath;
+// Function to create component files and folder structure
+async function createComponentFiles(selectedFolder: vscode.Uri, name: string, version: string, description: string) {
+    // Convert component name to PascalCase and KebabCase
+    const pascalCaseName = toPascalCase(name);
+    const kebabCaseName = toKebabCase(name);
+    const componentDir = vscode.Uri.file(`${selectedFolder.fsPath}/${pascalCaseName}`);
 
-        // Convert component name to PascalCase and KebabCase
-        const pascalCaseName = toPascalCase(name);
-        const kebabCaseName = toKebabCase(name);
-        const componentDir = vscode.Uri.file(`${rootPath}/${pascalCaseName}`);
-
+    try {
+        // Check if the component folder already exists
+        let componentDirExists = false;
         try {
-            // Check if the component folder already exists using try...catch
-            let componentDirExists = false;
-            try {
-                await vscode.workspace.fs.stat(componentDir);
-                componentDirExists = true;
-            } catch (error) {
-                componentDirExists = false;
-            }
-
-            if (!componentDirExists) {
-                // If component folder does not exist, create it
-                await vscode.workspace.fs.createDirectory(componentDir);
-            }
-
-            const versionDir = vscode.Uri.file(`${componentDir.fsPath}/${version}`);
-
-            // Check if the version folder already exists using try...catch
-            let versionDirExists = false;
-            try {
-                await vscode.workspace.fs.stat(versionDir);
-                versionDirExists = true;
-            } catch (error) {
-                versionDirExists = false;
-            }
-
-            if (versionDirExists) {
-                vscode.window.showInformationMessage(`Version ${version} already exists for component ${pascalCaseName}.`);
-                return;
-            }
-
-            // Create the version folder
-            await vscode.workspace.fs.createDirectory(versionDir);
-
-            // Create an empty .component.xml file in the version folder
-            const xmlFile = vscode.Uri.file(`${versionDir.fsPath}/${pascalCaseName}.component.xml`);
-            const emptyContent = Buffer.from('', 'utf8');
-            await vscode.workspace.fs.writeFile(xmlFile, emptyContent);
-
-            // Create src, test directories inside the version folder
-            const srcDir = vscode.Uri.file(`${versionDir.fsPath}/src`);
-            const testDir = vscode.Uri.file(`${versionDir.fsPath}/test`);
-            await vscode.workspace.fs.createDirectory(srcDir);
-            await vscode.workspace.fs.createDirectory(testDir);
-
-            // Create html/weBeans, js, less directories inside src
-            const htmlDir = vscode.Uri.file(`${srcDir.fsPath}/html`);
-            const jsDir = vscode.Uri.file(`${srcDir.fsPath}/js`);
-            const lessDir = vscode.Uri.file(`${srcDir.fsPath}/less`);
-            await vscode.workspace.fs.createDirectory(htmlDir);
-            await vscode.workspace.fs.createDirectory(jsDir);
-            await vscode.workspace.fs.createDirectory(lessDir);
-
-            const weBeansDir = vscode.Uri.file(`${htmlDir.fsPath}/weBeans`);
-            await vscode.workspace.fs.createDirectory(weBeansDir);
-
-            // --- Create the test/html and test/js directories ---
-            const testHtmlDir = vscode.Uri.file(`${testDir.fsPath}/html`);
-            const testJsDir = vscode.Uri.file(`${testDir.fsPath}/js`);
-            await vscode.workspace.fs.createDirectory(testHtmlDir);
-            await vscode.workspace.fs.createDirectory(testJsDir);
-
-            // --- Create the .weBean.html file ---
-            const weBeanFileContent = loadTemplate(version, 'Template.weBean.html')
-                .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
-                .replace(/\$\{kebabCaseName\}/g, kebabCaseName);
-
-            const weBeanFile = vscode.Uri.file(`${weBeansDir.fsPath}/${pascalCaseName}.weBean.html`);
-            await vscode.workspace.fs.writeFile(weBeanFile, Buffer.from(weBeanFileContent, 'utf8'));
-
-            // --- Create the .class.js file ---
-            const classJsFileContent = loadTemplate(version, 'Template.class.js')
-                .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
-                .replace(/\$\{kebabCaseName\}/g, kebabCaseName)
-                .replace(/\$\{componentDescription\}/g, description);
-
-            const classJsFile = vscode.Uri.file(`${jsDir.fsPath}/${pascalCaseName}.class.js`);
-            await vscode.workspace.fs.writeFile(classJsFile, Buffer.from(classJsFileContent, 'utf8'));
-
-            // --- Create the .less file ---
-            const lessFileContent = loadTemplate(version, 'Template.less')
-                .replace(/\$\{kebabCaseName\}/g, kebabCaseName);
-
-            const lessFile = vscode.Uri.file(`${lessDir.fsPath}/Default${pascalCaseName}.less`);
-            await vscode.workspace.fs.writeFile(lessFile, Buffer.from(lessFileContent, 'utf8'));
-
-            // --- Create the .html test file in test/html ---
-            const componentPath = `/${pascalCaseName}/${version}/${pascalCaseName}.component.xml`;
-            const testHtmlFileContent = loadTemplate(version, 'TemplateTest.html')
-                .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
-                .replace(/\$\{version\}/g, version)
-                .replace(/\$\{componentPath\}/g, componentPath);
-
-            const testHtmlFile = vscode.Uri.file(`${testHtmlDir.fsPath}/${pascalCaseName}.html`);
-            await vscode.workspace.fs.writeFile(testHtmlFile, Buffer.from(testHtmlFileContent, 'utf8'));
-
-            vscode.window.showInformationMessage(`Component ${pascalCaseName} version ${version} created successfully with .weBean.html, .class.js, .less, and test .html files!`);
-        } catch (err) {
-            const error = err as Error;  // Explicitly cast err to Error
-            vscode.window.showErrorMessage(`Error creating component files: ${error.message}`);
+            await vscode.workspace.fs.stat(componentDir);
+            componentDirExists = true;
+        } catch (error) {
+            componentDirExists = false;
         }
-    } else {
-        vscode.window.showErrorMessage('No workspace folder found.');
+
+        if (!componentDirExists) {
+            // If component folder does not exist, create it
+            await vscode.workspace.fs.createDirectory(componentDir);
+        }
+
+        const versionDir = vscode.Uri.file(`${componentDir.fsPath}/${version}`);
+
+        // Check if the version folder already exists
+        let versionDirExists = false;
+        try {
+            await vscode.workspace.fs.stat(versionDir);
+            versionDirExists = true;
+        } catch (error) {
+            versionDirExists = false;
+        }
+
+        if (versionDirExists) {
+            vscode.window.showInformationMessage(`Version ${version} already exists for component ${pascalCaseName}.`);
+            return;
+        }
+
+        // Create the version folder
+        await vscode.workspace.fs.createDirectory(versionDir);
+
+        // Create an empty .component.xml file in the version folder
+        const xmlFile = vscode.Uri.file(`${versionDir.fsPath}/${pascalCaseName}.component.xml`);
+        const emptyContent = Buffer.from('', 'utf8');
+        await vscode.workspace.fs.writeFile(xmlFile, emptyContent);
+
+        // Create src, test directories inside the version folder
+        const srcDir = vscode.Uri.file(`${versionDir.fsPath}/src`);
+        const testDir = vscode.Uri.file(`${versionDir.fsPath}/test`);
+        await vscode.workspace.fs.createDirectory(srcDir);
+        await vscode.workspace.fs.createDirectory(testDir);
+
+        // Create html/weBeans, js, less directories inside src
+        const htmlDir = vscode.Uri.file(`${srcDir.fsPath}/html`);
+        const jsDir = vscode.Uri.file(`${srcDir.fsPath}/js`);
+        const lessDir = vscode.Uri.file(`${srcDir.fsPath}/less`);
+        await vscode.workspace.fs.createDirectory(htmlDir);
+        await vscode.workspace.fs.createDirectory(jsDir);
+        await vscode.workspace.fs.createDirectory(lessDir);
+
+        const weBeansDir = vscode.Uri.file(`${htmlDir.fsPath}/weBeans`);
+        await vscode.workspace.fs.createDirectory(weBeansDir);
+
+        // --- Create the test/html and test/js directories ---
+        const testHtmlDir = vscode.Uri.file(`${testDir.fsPath}/html`);
+        const testJsDir = vscode.Uri.file(`${testDir.fsPath}/js`);
+        await vscode.workspace.fs.createDirectory(testHtmlDir);
+        await vscode.workspace.fs.createDirectory(testJsDir);
+
+        // --- Create the .weBean.html file ---
+        const weBeanFileContent = loadTemplate(version, 'Template.weBean.html')
+            .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
+            .replace(/\$\{kebabCaseName\}/g, kebabCaseName);
+
+        const weBeanFile = vscode.Uri.file(`${weBeansDir.fsPath}/${pascalCaseName}.weBean.html`);
+        await vscode.workspace.fs.writeFile(weBeanFile, Buffer.from(weBeanFileContent, 'utf8'));
+
+        // --- Create the .class.js file ---
+        const classJsFileContent = loadTemplate(version, 'Template.class.js')
+            .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
+            .replace(/\$\{kebabCaseName\}/g, kebabCaseName)
+            .replace(/\$\{componentDescription\}/g, description);
+
+        const classJsFile = vscode.Uri.file(`${jsDir.fsPath}/${pascalCaseName}.class.js`);
+        await vscode.workspace.fs.writeFile(classJsFile, Buffer.from(classJsFileContent, 'utf8'));
+
+        // --- Create the .less file ---
+        const lessFileContent = loadTemplate(version, 'Template.less')
+            .replace(/\$\{kebabCaseName\}/g, kebabCaseName);
+
+        const lessFile = vscode.Uri.file(`${lessDir.fsPath}/Default${pascalCaseName}.less`);
+        await vscode.workspace.fs.writeFile(lessFile, Buffer.from(lessFileContent, 'utf8'));
+
+        // --- Create the .html test file in test/html ---
+        const componentPath = `/${pascalCaseName}/${version}/${pascalCaseName}.component.xml`;
+        const testHtmlFileContent = loadTemplate(version, 'TemplateTest.html')
+            .replace(/\$\{pascalCaseName\}/g, pascalCaseName)
+            .replace(/\$\{version\}/g, version)
+            .replace(/\$\{componentPath\}/g, componentPath);
+
+        const testHtmlFile = vscode.Uri.file(`${testHtmlDir.fsPath}/${pascalCaseName}.html`);
+        await vscode.workspace.fs.writeFile(testHtmlFile, Buffer.from(testHtmlFileContent, 'utf8'));
+
+        vscode.window.showInformationMessage(`Component ${pascalCaseName} version ${version} created successfully with .weBean.html, .class.js, .less, and test .html files!`);
+    } catch (err) {
+        const error = err as Error;  // Explicitly cast err to Error
+        vscode.window.showErrorMessage(`Error creating component files: ${error.message}`);
     }
+}
+
+// Function to handle directory selection and component creation
+async function selectDirectoryAndCreateComponent(name: string, version: string, description: string) {
+    const uri = await vscode.window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: 'Select Component Directory',
+    });
+
+    if (!uri || uri.length === 0) {
+        vscode.window.showErrorMessage('No folder selected for component creation.');
+        return;
+    }
+
+    const selectedFolderUri = uri[0];
+    await createComponentFiles(selectedFolderUri, name, version, description);
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -191,13 +202,13 @@ export function activate(context: vscode.ExtensionContext) {
 
         // Handle messages from the webview
         panel.webview.onDidReceiveMessage(
-            message => {
+            async message => {
                 switch (message.command) {
                     case 'alert':
                         vscode.window.showInformationMessage(message.text);
                         return;
                     case 'createComponent':
-                        createComponentFiles(message.componentName, message.componentVersion, message.componentDescription);
+                        await selectDirectoryAndCreateComponent(message.componentName, message.componentVersion, message.componentDescription);
                         return;
                 }
             },
